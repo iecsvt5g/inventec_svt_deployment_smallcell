ac052cba89186179374506430533a6c409708657
ZL Chen <iec100535@gmail.com>
2023-04-19 11:16:14 +0800

    Updated: precondition_setting.sh

--- precondition_setting.sh
+++ precondition_setting.sh
@@ -8,19 +8,19 @@ Modified on 2023/04/13
 BLOCK
 
 function clean_log() {
-	echo "" > /root/check_time.log
+	echo "" > /root/zl/check_time.log
 }
 
 function version_check() {
 	if [[ $1 == "29" ]]
 	then
-		eeprom-tools -t $1 -r | grep uptime >> /root/check_time.log 2>&1
+		eeprom-tools -t $1 -r | grep uptime >> /root/zl/check_time.log 2>&1
 		echo "eeprom-tools -t $1 -r | grep uptime"
 	fi
 	if [[ $2 == mmcblk1p* ]] && [[ $3 == mmcblk1p* ]] 
 	then
-		lsblk | grep $2 >> /root/check_time.log 2>&1
-		lsblk | grep $3 >> /root/check_time.log 2>&1
+		lsblk | grep $2 >> /root/zl/check_time.log 2>&1
+		lsblk | grep $3 >> /root/zl/check_time.log 2>&1
 		echo "lsblk | grep $2, lsblk | grep $3"
 	fi
 }
@@ -28,11 +28,11 @@ function version_check() {
 function routing() {
 	if [[ $1 = */* ]] && [[ $2 = *.*.*.* ]]
 	then
-		/sbin/route del -net $1 gw $2 >> /root/check_time.log 2>&1
-		echo "/sbin/route del -net $1 gw $2 >> /root/check_time.log 2>&1"
+		/sbin/route del -net $1 gw $2 >> /root/zl/check_time.log 2>&1
+		echo "/sbin/route del -net $1 gw $2 >> /root/zl/check_time.log 2>&1"
 		sleep 1
-		/sbin/route add -net $1 gw $2 >> /root/check_time.log 2>&1
-		echo "/sbin/route add -net $1 gw $2 >> /root/check_time.log 2>&1"
+		/sbin/route add -net $1 gw $2 >> /root/zl/check_time.log 2>&1
+		echo "/sbin/route add -net $1 gw $2 >> /root/zl/check_time.log 2>&1"
 		sleep 1
 		echo "Add routing $1 gw $2"
 	else
@@ -49,19 +49,29 @@ function routing_gateway_check() {
 	else
 		echo "route -n | grep $1 Error"
 	fi
+}
+
+function dns_nameserver() {
+	if [[ $1 = nameserver ]]
+	then
+		echo "$1 $2" >> /etc/resolve.conf
+		echo "$1 $2 >> /etc/resolve.conf"
+	else
+		echo "$1 $2 >> /etc/resolve.conf Error"
+	fi
 	sync
 }
 
 function main() {
-	sleep 120
-	#sleep 1
 	clean_log
 	version_check "29" "mmcblk1p2" "mmcblk1p5"
-	routing "0.0.0.0/0" "172.32.3.5"
 	routing "192.168.99.0/24" "172.32.3.5"
 	routing "172.32.4.0/24" "172.32.3.5"
+	routing "0.0.0.0/0" "172.32.3.5"
 	routing_gateway_check "172.32.3.5"
+	dns_nameserver "nameserver" "8.8.8.8"
 }
 
+sleep 180
 main
 echo "Finished"
